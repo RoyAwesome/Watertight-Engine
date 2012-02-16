0d7e3c0e1757ac35677a47655cb6833ecc130593
RoyAwesome <madKylegar@gmail.com>
2012-02-15 17:25:02 -0800

    Added some test mods

---
+++ Modfiles/FileSystemMod/mod.lua
@@ -0,0 +1,10 @@
+local Mod = {};
+Mod.Name = "FileSystemMod";
+Mod.Author = {"RoyAwesome"};
+Mod.Version = "0.0.1";
+Mod.ServerMain = "sv_main.lua";
+Mod.ClientMain = "cl_main.lua";
+
+print("Mod Descriptor! File System!");
+
+RegisterMod(Mod);
