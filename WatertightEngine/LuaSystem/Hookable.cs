2f91eb7eaaa82fbe5d62dd820f81e6b4a2c7664a
RoyAwesome <madKylegar@gmail.com>
2012-02-16 00:21:22 -0800

    Don't crash the engine on lua error

--- WatertightEngine/LuaSystem/Hookable.cs
+++ WatertightEngine/LuaSystem/Hookable.cs
@@ -8,6 +8,8 @@ namespace Watertight.LuaSystem
 {
     abstract class Hookable
     {
+        static object[] empty = { };
+
         Dictionary<string, LuaFunction> hooks = new Dictionary<string, LuaFunction>();
 
         public void RegisterHook(string name, LuaFunction function)
@@ -16,11 +18,20 @@ namespace Watertight.LuaSystem
             hooks[name] = function;
         }
 
-        public object[] CallHook(string name, params object[] arguments)
+        protected object[] CallHook(string name, params object[] arguments)
         {
-            //Empty, just return
-            if (!hooks.ContainsKey(name)) return new object[]{ };
-            return hooks[name].Call(arguments);
+            //if empty, just return
+            if (!hooks.ContainsKey(name)) return empty;
+            try
+            {
+                return hooks[name].Call(arguments);
+            }
+            catch (LuaException e)
+            {
+                Console.WriteLine(e.Message);
+                if (e.InnerException != null) Console.WriteLine(e.InnerException.Message);
+                return empty;
+            }
         }
     }
 }
