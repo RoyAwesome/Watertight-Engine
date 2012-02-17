badf9a63a32e0594614d213c5c85b7500ac905aa
RoyAwesome <madKylegar@gmail.com>
2012-02-16 17:25:51 -0800

    Better commenting on sandbox types

--- sandbox.lua
+++ sandbox.lua
@@ -15,18 +15,22 @@ load = nil;
 loadfile = nil;
 loadstring = nil;
 
+--These aren't safe in certain situations
 rawequal = nil;
 rawget = nil;
 rawset = nil;
 
+--This changes global environment settings.  Don't want a mod to do this
 setfenv = nil;
 
+--Used to load other libraries 
 module = nil;
-
 package = nil;
 
+--Access OS level and FileIO.  Dangerous for untrusted code
 io = nil;
 os = nil;
 
+--Debug functions that let you access things you shouldn't
 debug = nil;
 newproxy = nil;
